a7837c5b3a37a1c0ff6bbec61dd900cc3e83666b
vitormbar <vitormb@gmail.com>
2026-09-19 21:31:59 +0000

    Adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <h1>
+      Deus, para a felicidade do homem, inventou a fé e o amor. O Diabo,
+      invejoso, fez o homem confundir fé com religião e amor com casamento.
+    </h1>
+  );
+}
+
+export default Home;
